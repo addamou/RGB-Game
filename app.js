949aadff5a8038fd4b1e0ca07166bc3ad70524fd
Addamou <adamou.halidou@codeloccol.org>
2020-06-23 16:11:37 +0100

    jeux version corriger

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var numBoites = 6;
-var colors = generateRandomColors(numBoites);
-var boite = document.querySelectorAll(".boite");
-var pickedColor = pickColor();
-var codeCouleur = document.getElementById("codeCouleur");
-var messageDisplay = document.querySelector("#message");
-var h2 = document.querySelector("h2");
-var resetButton = document.querySelector("#reset");
-var btnFacile = document.querySelector("#btnFacile");
-var btnDur = document.querySelector("#btnDur");
+let numBoites = 6;
+let colors = generateRandomColors(numBoites);
+let boite = document.querySelectorAll(".boite");
+let pickedColor = pickColor();
+let codeCouleur = document.getElementById("codeCouleur");
+let messageDisplay = document.querySelector("#message");
+let h2 = document.querySelector("h2");
+let resetButton = document.querySelector("#reset");
+let btnFacile = document.querySelector("#btnFacile");
+let btnDur = document.querySelector("#btnDur");
 
 
 btnFacile.addEventListener("click", function () {
@@ -17,7 +17,7 @@ btnFacile.addEventListener("click", function () {
     colors = generateRandomColors(numBoites);
     pickedColor = pickColor();
     codeCouleur.textContent = pickedColor;
-    for (var i = 0; i < boite.length; i++) {
+    for (let i = 0; i < boite.length; i++) {
         if (colors[i]) {
             boite[i].style.background = colors[i];
         } else {
@@ -33,7 +33,7 @@ btnDur.addEventListener("click", function () {
     colors = generateRandomColors(numBoites);
     pickedColor = pickColor();
     codeCouleur.textContent = pickedColor;
-    for (var i = 0; i < boite.length; i++) {
+    for (let i = 0; i < boite.length; i++) {
         boite[i].style.background = colors[i];
         boite[i].style.display = "block";
     }
@@ -49,7 +49,7 @@ resetButton.addEventListener("click", function () {
     this.textContent = "Nouvelle Couleur";
     messageDisplay.textContent = "";
     //changer la couleur des boites
-    for (var i = 0; i < boite.length; i++) {
+    for (let i = 0; i < boite.length; i++) {
         boite[i].style.background = colors[i];
     }
     h2.style.background = "teal";
@@ -57,13 +57,13 @@ resetButton.addEventListener("click", function () {
 
 codeCouleur.textContent = pickedColor;
 
-for (var i = 0; i < boite.length; i++) {
+for (let i = 0; i < boite.length; i++) {
     //ajouter des couleurs initiales aux boites
     boite[i].style.background = colors[i];
     //ajouter des options de clic aux boites
     boite[i].addEventListener("click", function () {
         //saisir la couleur de la boite cueillie
-        var clickedColor = this.style.background;
+        let clickedColor = this.style.background;
         //comparer la couleur à la couleur choisie
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Félicitation!";
@@ -79,7 +79,7 @@ for (var i = 0; i < boite.length; i++) {
 
 function changeColors(color) {
         //parcourir tous les boites
-    for (var i = 0; i < boite.length; i++) {
+    for (let i = 0; i < boite.length; i++) {
         //changer chaque couleur pour correspondre à la couleur donnée
         boite[i].style.background = color;
     }
@@ -87,15 +87,15 @@ function changeColors(color) {
 }
 
 function pickColor() {
-    var random = Math.floor(Math.random() * colors.length)
+    let random = Math.floor(Math.random() * colors.length)
     return colors[random];
 }
 
 function generateRandomColors(num) {
     //nouveau tableau
-    var arr = []
+    let arr = []
     //add num random colors to array
-    for (var i = 0; i < num; i++) {
+    for (let i = 0; i < num; i++) {
         arr.push(randomColor())
         //get random color and push into array
     }
@@ -105,11 +105,11 @@ function generateRandomColors(num) {
 
 function randomColor() {
     //choisi le "rouge" from 0-255
-    var r = Math.floor(Math.random() * 256)
+    let r = Math.floor(Math.random() * 256)
     //choisi le "vert" from 0-255
-    var g = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
     //choisi le "blue" from 0-255
-    var b = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
     //code finale
     return "rgb(" + r + ", " + g + ", " + b + ")";
 }
